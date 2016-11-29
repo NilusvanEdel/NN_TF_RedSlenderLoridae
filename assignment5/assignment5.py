59c7bc2cd2b2c5e26882576b4f9e8cb0a1b4e973
Linus Edelkott <ledelkott@uni-osnabrueck.de>
2016-11-29 15:34:45 +0100

    implemented network from ass4

--- assignment5/assignment5.py
+++ assignment5/assignment5.py
@@ -75,63 +75,98 @@ def getMiniBatch(size):
     return returnArray
 
 
-# only the code of assignment 4 so far
-x = tf.placeholder(tf.float32, [None, 784])
-desired = tf.placeholder(tf.int64, [None])
-weights = tf.Variable(tf.zeros([784, 10]))
-biases = tf.Variable(tf.zeros([10]))
-logits = tf.matmul(x, weights) + biases
-crossEntropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, desired)
+# basically only the code of assignment 4 so far (added saverclass)
+x = tf.placeholder(tf.float32, shape = [None, 28, 28, 1])
+desired = tf.placeholder(tf.int64, shape = [None])
+# First convolutional layer
+initval = tf.truncated_normal([5, 5, 1, 32], stddev = 0.1)
+kernels1 = tf.Variable(initval)
+bias1 = tf.Variable(tf.constant(0.1, shape = [32]))
+conv1 = tf.nn.conv2d(x, kernels1, strides = [1, 1, 1, 1], padding = "SAME")
+actv1 = tf.nn.tanh(conv1 + bias1)
+pool1 = tf.nn.max_pool(actv1, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
+# Second convolutional layer
+kernels2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev = 0.1))
+bias2 = tf.Variable(tf.constant(0.1, shape = [64]))
+conv2 = tf.nn.conv2d(pool1, kernels2, strides = [1, 1, 1, 1], padding = "SAME")
+actv2 = tf.nn.tanh(conv2 + bias2)
+pool2 = tf.nn.max_pool(actv2, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
+# Vectorize the output of the second convolutional layer
+pool2Flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+# Fully connected feed forward layer
+initval = tf.truncated_normal([7 * 7 * 64, 1024], stddev = 0.1)
+weights1 = tf.Variable(initval)
+ffnnbias1 = tf.Variable(tf.constant(0.1, shape = [1024]))
+ffnn1act = tf.nn.tanh(tf.matmul(pool2Flat, weights1) + ffnnbias1)
+# Fully connected readout layer
+weights2 = tf.Variable(tf.truncated_normal([1024, 10], stddev = 0.1))
+ffnnbias2 = tf.Variable(tf.constant(0.1, shape = [10]))
+ffnn2act = tf.matmul(ffnn1act, weights2) + ffnnbias2
+crossEntropy = tf.nn.sparse_softmax_cross_entropy_with_logits(ffnn2act, desired)
 crossEntropy = tf.reduce_mean(crossEntropy)
-learningRate = 1e-5
-gdsOptimizer = tf.train.GradientDescentOptimizer(learningRate)
-trainingStep = gdsOptimizer.minimize(crossEntropy)
-accuracy = tf.equal(tf.argmax(tf.nn.softmax(logits), 1), desired)
+train_step = tf.train.AdamOptimizer(0.00007).minimize(crossEntropy)
+# Calculate the accuracy
+accuracy = tf.equal(tf.argmax(tf.nn.softmax(ffnn2act), 1), desired)
 accuracy = tf.reduce_mean(tf.cast(accuracy, tf.float32))
-trainingSteps = 700
+trainingSteps = 3
+plotStepSize = 1
 miniBatchSize = 300
-plotStepSize = 25
-trainingAccuracy = np.ones(trainingSteps)
-validationAccuracy = np.ones(trainingSteps)
-
+trainingAccurcies = np.ones(trainingSteps)
+validationAccuracies = np.ones(trainingSteps)
+trainingCrossEntropies = np.zeros(trainingSteps)
+validationCrossEntropies = np.zeros(trainingSteps)
+accFig, accAx = plt.subplots(1,1)
+ceFig, ceAx = plt.subplots(1,1)
+actFig, actAx = plt.subplots(10,1)
 accuracyFigure, accuracyAxis = plt.subplots(1,1)
 weightFigure, weightAxes = plt.subplots(2,5)
 
 with tf.Session() as session:
     # activate if run the first time, deactivate otherwise
-    # session.run(tf.initialize_all_variables())
+    session.run(tf.initialize_all_variables())
     saver = tf.train.Saver()
     # activate to restore the old variables
-    saver.restore(session, "./simple-ffnn.ckpt")
-
+    # saver.restore(session, "./simple-ffnn.ckpt")
+    cnt = 0
     for step in range(trainingSteps):
-        if step % 100 == 0 or step == (trainingSteps-1):
+        print (cnt)
+        cnt += 1
+        if step % 100 == 0 or step == (trainingSteps - 1):
             saver.save(session, "./simple-ffnn.ckpt")
         images, labels = getMiniBatch(miniBatchSize)
-        images = images.reshape([-1, 784])
-        trainingAccuracy[step], _ = session.run([accuracy, trainingStep], feed_dict={x: images, desired: labels})
-
-        if step % plotStepSize == 0 or step == trainingSteps - 1:
+        images = images.reshape([-1, 28, 28, 1])
+        trainingAccurcies[step], trainingCrossEntropies[step], _ = session.run([accuracy, crossEntropy, train_step],
+                                                                               feed_dict={x: images, desired: labels})
+        print("Oh my fucking god, it's a dinosaur")
+        if step % plotStepSize == 0 or step == 0 or step == trainingSteps - 1:
             images, labels = validationData, validationLabels
-            images = images.reshape([-1, 784])
-
-            _accuracy, _weights = session.run([accuracy, weights], feed_dict={x: images, desired: labels})
+            images = images.reshape([-1, 28, 28, 1])
+            validationAccuracy, validationcrossEntropy, outputActivation = session.run(
+                [accuracy, crossEntropy, ffnn2act], feed_dict={x: images, desired: labels})
             if step != trainingSteps - 1:
-                validationAccuracy[step:step + plotStepSize] = [_accuracy] * plotStepSize
-            accuracyAxis.cla()
-            accuracyAxis.plot(trainingAccuracy, color='b')
-            accuracyAxis.plot(validationAccuracy, color='r')
-            accuracyFigure.canvas.draw()
+                validationAccuracies[step:step + plotStepSize] = [validationAccuracy] * plotStepSize
+                validationCrossEntropies[step:step + plotStepSize] = [validationcrossEntropy] * plotStepSize
+            print("do we even get here?")
+            accAx.cla()
+            accAx.plot(trainingAccurcies, color='b')
+            accAx.plot(validationAccuracies, color='r')
+            accFig.canvas.draw()
 
-            for i in range(10):
-                weight = _weights[:, i].reshape(28, 28)
-                weightAxes[i // 5, i % 5].cla()
-                weightAxes[i // 5, i % 5].matshow(weight, cmap=plt.get_cmap('bwr'))
-            weightFigure.canvas.draw()
-
+            ceAx.cla()
+            ceAx.plot(trainingCrossEntropies, color='b')
+            ceAx.plot(validationCrossEntropies, color='r')
+            ceFig.canvas.draw()
+            outputActivation = outputActivation.T
+            for i, ax in enumerate(actAx):
+                ax.cla()
+                ax.set_xticklabels([])
+                ax.set_yticklabels([])
+                ax.matshow(np.matrix(outputActivation[i]), extent=[0, 6000, 0, 1], aspect='auto',
+                           cmap=plt.get_cmap('Blues'))
+            actFig.canvas.draw()
+            print("just do it")
     images, labels = testData, testLabels
-    images = images.reshape([-1, 784])
-    _accuracy = session.run(accuracy, feed_dict={x: images, desired: labels})
-    plt.show()
-    print("Test accuracy: ", _accuracy)
-
+    images = images.reshape([-1, 28, 28, 1])
+    testAccuracy = session.run(accuracy, feed_dict={x: images, desired: labels})
+    print("Final: ", testAccuracy)
+plt.show()
